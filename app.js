61200fed35122c4d10fa421eea070d04ce7208df
kimjiseok0587 <kim31334519@naver.com>
2026-05-09 14:05:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,37 @@
 import { db } from "./firebase.js";
-
 import {
   collection,
   addDoc
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
 async function login() {
-
   const name = document.getElementById("name").value;
+  const baptismName = document.getElementById("baptismName").value;
 
-  const baptismName =
-    document.getElementById("baptismName").value;
-
-
-  if (name === "" || baptismName === "") {
-
-    alert("이름과 세례명을 입력해주세요.");
-
+  if (!name || !baptismName) {
+    alert("이름과 세례명을 모두 입력해주세요.");
     return;
   }
 
-
-  // Firebase 저장
   try {
-
     await addDoc(collection(db, "participants"), {
-
       name: name,
-
       baptismName: baptismName,
-
       createdAt: new Date()
-
     });
 
     console.log("참가자 저장 완료");
-
   } catch (error) {
-
     console.error("저장 실패:", error);
-
+    alert("저장 중 오류가 발생했습니다.");
+    return;
   }
 
-
   localStorage.setItem("name", name);
-
   localStorage.setItem("baptismName", baptismName);
 
-
   window.location.href = "map.html";
-
 }
-
 
 // HTML 버튼에서 사용할 수 있게 등록
 window.login = login;
